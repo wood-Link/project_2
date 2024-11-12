2d38a746450ba44d9500fc1b39eef325f7c109e6
MatinKim <ghks6455@gmail.com>
2024-11-12 14:09:22 +0900

    리액트 설치?

---
+++ public/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  function Main() {
+    return (
+      <>
+        <main></main>
+      </>
+    );
+  }
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ public/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <h1>footer</h1>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
