5bdd36ab36e17ab003e578e5171e369c1f8dfc8b
yh97777 <yh97777@naver.com>
2024-11-14 11:31:53 +0900

    main추가

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,20 +1,18 @@
 import "./App.css";
 import Header from "./components/header/Header";
-
 import Teg from "./components/Teg/Teg";
-
 import Review from "./components/Review/Review";
 import Question from "./components/Question/Question";
 import Footer from "./components/Footer/Footer";
-
 import ShareLink from "./components/ShareLink/ShareLink";
 import ShareLinkTab from "./components/ShareLinkTab/ShareLinkTab";
-
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
       <Teg />
       <ShareLink />
       <ShareLinkTab />
