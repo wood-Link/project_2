b613dbd7ecc7cf8873a15c27246b8cea8fef6057
MatinKim <ghks6455@gmail.com>
2024-11-18 13:13:37 +0900

    폼 연결

--- public/src/components/ShareLinkTab/ShareLinkTab.jsx
+++ public/src/components/ShareLinkTab/ShareLinkTab.jsx
@@ -3,16 +3,18 @@ import "./ShareLinkTab.css";
 import { Share_link_form } from "./Share_link_form";
 import { Share_link_card } from "./Share_link_card";
 import pageButton from "../../assets/pageButton.png";
+import dummyData from "../ShareLink/dummyData";
 
-function ShareLinkTab({ setContents }) {
+function ShareLinkTab({ setContents, productId, category }) {
   const [userData, setUserData] = useState({
     userName: "",
     userTel: "",
     street: "",
     address: "",
-    isSending: false,
   });
 
+  const productData = dummyData[category][productId];
+  console.log(dummyData[category][productId]);
   return (
     <section className="ShareLinkTabBoxMom">
       <section className="ShareLinkTabBox">
@@ -20,11 +22,19 @@ function ShareLinkTab({ setContents }) {
           <li className="tabTitle">나눔신청</li>
         </div>
         <section className="shareApply">
-          <Share_link_card />
-          <Share_link_form userData={userData} setUserData={setUserData} />
+          <Share_link_card product={productData} />
+          <Share_link_form
+            userData={userData}
+            setUserData={setUserData}
+            productData={productData}
+          />
         </section>
         <div className="pageButtonWrap">
-          <img src={pageButton} alt="페이지 버튼" onClick={() => setContents(false)} />
+          <img
+            src={pageButton}
+            alt="페이지 버튼"
+            onClick={() => setContents(false)}
+          />
         </div>
       </section>
     </section>
