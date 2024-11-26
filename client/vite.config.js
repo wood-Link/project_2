533e00ac98b53413e7e35bbd8fdd5ed584a30879
MatinKim <ghks6455@gmail.com>
2024-11-26 13:34:08 +0900

    페키지 콘피그 넷틀리파이 변경

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,35 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import fs from "fs/promises";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  resolve: {
-    alias: {
-      src: resolve(__dirname, "src"),
-    },
-  },
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        {
-          name: "load-js-files-as-jsx",
-          setup(build) {
-            build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
-              loader: "jsx",
-              contents: await fs.readFile(args.path, "utf8"),
-            }));
-          },
-        },
-      ],
-    },
-  },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        format: "es",
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash][extname]",
+      },
+    },
+  },
+  base: "/",
+  server: {
+    historyApiFallback: true,
+  },
 });
